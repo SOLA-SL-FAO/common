9857e7c2c716de25c8921dc59dcfa26fec462431
amcdowell <armcdowell@hotmail.com>
2014-09-26 18:07:01 +1200

    Added new Service Checklist Task

--- src/main/java/org/sola/common/RolesConstants.java
+++ src/main/java/org/sola/common/RolesConstants.java
@@ -110,6 +110,7 @@ public class RolesConstants {
     public static final String CLASSIFICATION_SECRET = "04SEC_Secret";
     public static final String CLASSIFICATION_TOPSECRET = "05SEC_TopSecret";
     public static final String CLASSIFICATION_SUPPRESSION_ORDER = "10SEC_SuppressOrd";
-    
+    // SERVICES
+     public static final String SERVICE_START_CHECKLIST = "checklist";
 
 }
